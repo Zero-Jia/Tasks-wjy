356e467348c9152496d46fb4d6f719e3f755d4fd
Zero-Jia <2563297648@qq.com>
2025-02-22 20:28:07 +0800

    Add files via upload

---
+++ assignment1/Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入第一个数的值：");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("请输入运算符：");
+            string s = Console.ReadLine();
+            Console.Write("请输入第二个数的值：");
+            double b = double.Parse(Console.ReadLine());
+            if (s == "+")
+            {
+                Console.WriteLine("最后结果:"+(a + b));
+            }
+            else if (s == "-")
+            {
+                Console.WriteLine("最后结果:" + (a - b));
+            }
+            else if (s == "*")
+            {
+                Console.WriteLine("最后结果:" + (a * b));
+            }
+            else if (s=="/")
+            {
+                Console.WriteLine("最后结果:" + (a / b));
+            }
+            else
+            {
+                Console.WriteLine("没有该运算符");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ assignment1/CalculatorApp/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(textBox1.Text);
+            double num2 = double.Parse(textBox2.Text);
+            string op = comboBox1.SelectedItem.ToString();
+            double result = 0;
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0)
+                        result = num1 / num2;
+                    else
+                        MessageBox.Show("除数不能为零！");
+                    break;
+                esle:
+                    MessageBox.Show("请输入有效运算符！");
+                        break;
+            }
+            label1.Text = result.ToString();
+        }
+    }
+}
